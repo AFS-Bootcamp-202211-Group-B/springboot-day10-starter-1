3ed11ddee8a4fa63a59cc16bb780495890ac7d56
leungpo <polly.leung@oocl.com>
2022-12-09 11:01:22 +0800

    feat: use EmployeeRequest instead of Employee and create employeeMapper

---
+++ src/main/java/com/rest/springbootemployee/controller/mapper/EmployeeMapper.java
@@ -0,0 +1,22 @@
+package com.rest.springbootemployee.controller.mapper;
+
+
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import com.rest.springbootemployee.controller.dto.EmployeeRequest;
+import com.rest.springbootemployee.entity.Employee;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmployeeMapper {
+    public Employee toEntity(EmployeeRequest employeeRequest){
+        Employee employee = new Employee();
+//        employee.setName(employeeRequest.getName());
+//        employee.setAge(employeeRequest.getAge());
+//        employee.setGender(employeeRequest.getGender());
+//        employee.setSalary(employeeRequest.getSalary());
+
+        BeanUtils.copyProperties(employeeRequest,employee);
+        return employee;
+    }
+}

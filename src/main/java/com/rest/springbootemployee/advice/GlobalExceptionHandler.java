b2a6a91d45b20f3f09c823c829a25b823257da89
leungpo <polly.leung@oocl.com>
2022-12-10 11:38:54 +0800

    Revert "feat: add InvalidIdException for getById in CompanyController"
    This reverts commit 9638bc3d974daba260983cc7706bae3762b0f32c.

--- src/main/java/com/rest/springbootemployee/advice/GlobalExceptionHandler.java
+++ src/main/java/com/rest/springbootemployee/advice/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.rest.springbootemployee.advice;
 
-import com.rest.springbootemployee.exception.InvalidIdException;
 import com.rest.springbootemployee.exception.NoCompanyFoundException;
 import com.rest.springbootemployee.exception.NoEmployeeFoundException;
 import org.springframework.http.HttpStatus;
@@ -15,12 +14,6 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse method(Exception exception) {
         return new ErrorResponse(HttpStatus.NOT_FOUND.value(), exception.getMessage());
-    }
-
-    @ExceptionHandler(InvalidIdException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleInvalidIdException(Exception exception) {
-        return new ErrorResponse(HttpStatus.BAD_REQUEST.value(),exception.getMessage());
     }
 
 
--- src/main/java/com/rest/springbootemployee/exception/InvalidIdException.java
+++
@@ -1,7 +0,0 @@
-package com.rest.springbootemployee.exception;
-
-public class InvalidIdException extends RuntimeException{
-    public InvalidIdException() {
-        super("Invalid Id");
-    }
-}
